eee7b3e680e6fb9598494a2777b3ba600df14215
GIDYBOY <ezeukofrank@gmail.com>
2025-08-24 17:52:54 +0100

    remove files for a different deployment

--- dist/developer-admin-panel/src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.baseApiUrl = void 0;
-exports.baseApiUrl = "http://localhost:4000/api/v1";
--- src/developer-admin-panel/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export {default as Header} from './header';
-export {default as Signin} from './signin';
-export {default as Dashboard} from './dashboard';
-
